e0698d10bacd410eacce4b8cf4dc73bad1ff2bd2
Nick Mattheij <nmattheij@live.nl>
2022-11-29 19:50:58 +0100

    Setup to prepare for the first assignment

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Empty()
+{
+    Console.WriteLine("");
+}
+void Title(string title)
+{
+    const int titleLength = 60;
+    Console.WriteLine(new string('=', titleLength));
+    Console.Write(new string('=', (titleLength - title.Length) / 2 - 2));
+    Console.Write($" {title} ");
+    Console.Write(new string('=', titleLength - 2 - title.Length - ((titleLength - title.Length) / 2 - 2)));
+    Console.Write("\n");
+    Console.WriteLine(new string('=', titleLength));
+}
+
+Title("Hello");
+Empty();
+
+var assignmentReader = new AssignmentReader01("Input/01.txt");
+
+Title("Input");
+assignmentReader.PrintInput();
+Empty();
+
+Title("Output");
+assignmentReader.PrintOutput();
+Empty();
+Empty();
+
+Console.WriteLine("Saving output...");
+assignmentReader.SaveOutput("Output/01.txt");
+Console.WriteLine("Done, bye!");
+
+Console.ReadLine();
