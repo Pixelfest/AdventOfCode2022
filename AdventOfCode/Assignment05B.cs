ce3659f2b53466205eeeef6adbc03ea3ad0bb1da
Nick Mattheij <nmattheij@live.nl>
2022-12-05 06:36:45 +0100

    Day 5

---
+++ AdventOfCode/Assignment05B.cs
@@ -0,0 +1,86 @@
+﻿using System.Data.SqlTypes;
+using System.Text;
+
+namespace AdventOfCode;
+
+public class Assignment05B : Assignment, IAmAnAssignment
+{
+    public Assignment05B()
+    {
+        Load("Input/05.txt");
+    }
+
+    private Stack<Char>[]? _list = null;
+    bool start = true;
+
+    protected override void ReadLine(string line)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            start = false;
+
+            for (int i = 0; i < _list.Length; i++)
+            {
+                var reverse = new Stack<Char>();
+                
+                while (_list[i].Count != 0)
+                {
+                    reverse.Push(_list[i].Pop());
+                }
+
+                _list[i] = reverse;
+            }
+
+            return;
+        }
+
+        if (line[1] == '1')
+            return;
+        
+        if (start)
+        {
+            if (_list == null)
+            {
+                _list = new Stack<Char>[(line.Length + 1) / 4];
+                for (int i = 0; i < _list.Length; i++)
+                {
+                    _list[i] = new Stack<char>();
+                }
+            }
+
+            for (int i = 1; i < line.Length; i += 4)
+            {
+                if (char.IsLetter(line[i]))
+                {
+                    _list[(i - 1) / 4].Push(line[i]);
+                }
+            }
+        }
+        else
+        {
+            var split = line.Split(" ");
+            int count = int.Parse(split[1]);
+            int from =  int.Parse(split[3]) - 1;
+            int to =  int.Parse(split[5]) - 1;
+
+            Stack<Char> temp = new Stack<char>();
+                
+            for (int i = 0; i < count; i++)
+            {
+                
+                var value = _list[from].Pop();
+                temp.Push(value);
+            }
+
+            while (temp.Count > 0)
+            {
+                _list[to].Push(temp.Pop());
+            }
+        }
+    }
+    
+    public override void Process()
+    {
+        Output = string.Concat(_list.Select(stack => stack.Peek()));
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,7 +4,7 @@ static class Program
 {
     public static void Main(string[] args)
     {
-        Run(new Assignment04B());
+        Run(new Assignment05B());
 
         Console.WriteLine("Bye!");
     }
